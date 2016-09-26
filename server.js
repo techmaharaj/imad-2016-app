5bc80310f8f2ca46bcb06ca733a29ae6c2f50615
techmaharaj <sharma.rockstar@gmail.com>
2016-09-26 09:24:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+// Adding url for profile page
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
